992bc6f89d50b573c2e21b69be39155832038605
madruga665 <madruga665@hotmail.com>
2021-02-28 22:26:33 -0300

    add storage via cookies

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,13 +1,9 @@
 import '../styles/pages/global.css'
 
-import { ChallengesProvider } from '../contexts/ChallengesContext'
-
 function MyApp({ Component, pageProps }) {
   return(
-    <ChallengesProvider>
         <Component {...pageProps} />
-    </ChallengesProvider>
-  ) 
+  )      
 }
 
 export default MyApp
